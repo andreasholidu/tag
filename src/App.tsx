6eb6db2dcd1a88fafd794bb88ddeac9b3b130b82
andreasholidu <andreas.palmer+github@holidu.com>
2024-08-27 16:49:38 +0200

    Some more improvements

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
           buttonText="Modal 1"
           titleText="First Modal"
           modalCTAButtonText="Save and close"
-          modalContent="<p>This is the content for the first modal.</p>"
+          modalContent='<p>Hello this is the first modal content.</p>'
           showOptionalButton={true}
           optionalButtonText="Skip"
           optionalButtonAction={() => alert('First Action Clicked')}
@@ -18,7 +18,17 @@ function App() {
           buttonText="Modal 2"
           modalCTAButtonText="Close"
           titleText="Second Modal"
-          modalContent="<p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p><p>This is the content for the second modal.</p>"
+          modalContent='<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
+
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
+          
+          Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
+          
+          Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.   
+          
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.   
+          
+          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur</p>'
           showOptionalButton={false}
         />
 
